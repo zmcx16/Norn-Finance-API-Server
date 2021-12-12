bb012abb6b1359fb2d20aea3a18b0ae84a23b2cd
zmcx16 <zmcx16@gmail.com>
2021-12-12 21:40:15 +0800

    add test

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+
+# rate limiter
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+# gzip
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+
+# cors
+origins = ["http://localhost:5000", "https://project.zmcx16.moe", "https://norn-stockscreener.zmcx16.moe"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
-def hello():
-    return {"message":"Hello TutLinks.com"}
+@limiter.limit("10/minute")
+def hello(request: Request, response: Response):
+    return {"msg": "Hello Norn"}
