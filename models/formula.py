e7ef6fabc0bddfdc8a6df636f9df9133e0f29690
zmcx16 <zmcx16@gmail.com>
2022-01-02 00:49:26 +0800

    add ewma_historical_volatility

--- models/formula.py
+++ models/formula.py
@@ -8,21 +8,24 @@ def historical_volatility(quotes, trading_days):
 
 
 def avg_historical_volatility(data, period=21, trading_days=252):
-    total_cnt = len(data) - period + 1
+    total_cnt = len(data) - period + 1 - 1  # -1 for shift data
     vol_sum = 0
     for i in range(total_cnt):
-        vol_sum = vol_sum + historical_volatility(data[i:i+period], trading_days)
+        vol_sum = vol_sum + historical_volatility(data[i:i+period+1], trading_days)  # +1 for shift data
 
     return vol_sum / total_cnt
 
 
-"""
-EWMA
-def exp_weight_historical_volatility(data, period=21, trading_days=252, ):
-    total_cnt = len(data) - period + 1
-    vol_sum = 0
-    for i in range(total_cnt):
-        vol_sum = vol_sum + historical_volatility(data[i:i+period], trading_days)
+def ewma_historical_volatility(data, period=21, trading_days=252, p_lambda=0.94):
+    # a_i = l^(i-1) * (1-l) / (1-l^n)
+    if p_lambda >= 1.0:
+        p_lambda = 0.9999999999999999  # prevent divided by 0 if p_lambda = 1
 
-    return vol_sum / total_cnt
-"""
+    total_cnt = len(data) - period + 1 - 1  # -1 for shift data
+    output = 0
+    for i in range(total_cnt):
+        alpha_i = total_cnt - i - 1  # i: start->latest data, alpha_i: latest->start data factor
+        p_alpha_i = (p_lambda**alpha_i) * (1-p_lambda) / (1-p_lambda**total_cnt)
+        output = output + p_alpha_i * historical_volatility(data[i:i+period+1], trading_days)
+
+    return output
--- test/models/test_formula.py
+++ test/models/test_formula.py
@@ -11,4 +11,36 @@ def test_historical_volatility():
                   15.406426, 16.077011, 14.973518, 14.948055, 14.812241, 14.829216, 14.608518, 14.387821, 14.574565,
                   14.014332, 14.218052, 13.708749, 14.209564]))
     output = formula.historical_volatility(quotes, 252)
-    assert output == approx(0.468, rel=1e-3)
+    assert output == approx(0.46795, rel=1e-5)
+
+
+def test_avg_historical_volatility():
+    data = pd.Series(
+        np.array([14.387821, 14.226541, 14.769797, 15.015962, 15.414914, 16.068521, 16.060032, 15.796894, 15.881777,
+                  15.406426, 16.077011, 14.973518, 14.948055, 14.812241, 14.829216, 14.608518, 14.387821, 14.574565,
+                  14.014332, 14.218052, 13.708749, 14.209564, 13.903981, 14.336889, 14.005842]))
+
+    output = formula.avg_historical_volatility(data, 21, 252)
+    assert output == approx(0.468298, rel=1e-5)
+
+
+def test_ewma_historical_volatility_lambda_approx1():
+    data = pd.Series(
+        np.array([14.387821, 14.226541, 14.769797, 15.015962, 15.414914, 16.068521, 16.060032, 15.796894, 15.881777,
+                  15.406426, 16.077011, 14.973518, 14.948055, 14.812241, 14.829216, 14.608518, 14.387821, 14.574565,
+                  14.014332, 14.218052, 13.708749, 14.209564, 13.903981, 14.336889, 14.005842]))
+
+    # lambda ~= 1 mean avg_historical_volatility
+    output = formula.ewma_historical_volatility(data, 21, 252, 1.0)
+    assert output == approx(formula.avg_historical_volatility(data, 21, 252), rel=1e-3)
+
+
+def test_ewma_historical_volatility_lambda_0():
+    data = pd.Series(
+        np.array([14.387821, 14.226541, 14.769797, 15.015962, 15.414914, 16.068521, 16.060032, 15.796894, 15.881777,
+                  15.406426, 16.077011, 14.973518, 14.948055, 14.812241, 14.829216, 14.608518, 14.387821, 14.574565,
+                  14.014332, 14.218052, 13.708749, 14.209564, 13.903981, 14.336889, 14.005842]))
+
+    # lambda = 0 mean latest historical_volatility
+    output = formula.ewma_historical_volatility(data, 21, 252, 0)
+    assert output == formula.historical_volatility(data[len(data)-21-1:len(data)], 252)
