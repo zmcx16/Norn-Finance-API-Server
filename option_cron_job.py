68429017779ef45e90b34db3f29604cc43963de2
Ken You <zmcx16@gmail.com>
2022-01-22 18:59:16 +0800

    Update option_cron_job.py

--- option_cron_job.py
+++ option_cron_job.py
@@ -18,8 +18,8 @@ afscreener_url = os.environ.get(
     "AF_URL", "")
 afscreener_token = os.environ.get("AF_TOKEN", "")
 
-DELAY_TIME_SEC = 3
-THREAD_CNT = 2
+DELAY_TIME_SEC = 0
+THREAD_CNT = 1
 
 nf_client = TestClient(app)
 api_thread_lock = threading.Lock()
