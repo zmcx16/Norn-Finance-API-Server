8dbc0bd15ef1c90e99f5d86b1a810079130720bf
Ken You <zmcx16@gmail.com>
2023-01-25 01:51:39 +0800

    Update esg_cron_job.py

--- esg_cron_job.py
+++ esg_cron_job.py
@@ -85,7 +85,7 @@ def get_stock_data_by_browser(symbol, retry):
         try:
             driver.get("https://hk.finance.yahoo.com/quote/" + symbol + "?p=" + symbol)
             time.sleep(DELAY_TIME_SEC)
-            if "finance.yahoo.com" not in driver.current_url:
+            if "err=404" in driver.current_url:
                 logging.warning('auto redirect to ' + driver.current_url + ', skip it')
                 return None
             root_app_main = driver.execute_script("return App.main")
