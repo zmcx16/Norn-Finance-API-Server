150364841e3d62a152ba8f563c993b9a899a38ee
zmcx16 <zmcx16@gmail.com>
2023-02-08 23:08:51 +0800

    add sync recommendation_data

--- yahoo_finance_cron_job.py
+++ yahoo_finance_cron_job.py
@@ -12,8 +12,7 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-afscreener_url = os.environ.get(
-    "AF_URL", "")
+afscreener_url = os.environ.get("AF_URL", "")
 afscreener_token = os.environ.get("AF_TOKEN", "")
 
 
@@ -107,13 +106,13 @@ def get_stock_data_by_browser(symbol, retry):
     sys.exit(1)
 
 
-def update_db(output):
+def update_db(output, api):
     logging.info(f'update to db, output = {output}')
     if len(output["data"]) > 0:
         # update data to server
         param = {
             'code': afscreener_token,
-            'api': 'update-esg-data'
+            'api': api
         }
         encoded_args = urlencode(param)
         query_url = afscreener_url + '?' + encoded_args
@@ -134,6 +133,37 @@ def update_db(output):
     output["data"] = {}
 
 
+def get_af_common_data(api, retry):
+    for r in range(retry):
+        try:
+            param = {
+                'code': afscreener_token,
+                'api': api
+            }
+            encoded_args = urlencode(param)
+            query_url = afscreener_url + '?' + encoded_args
+
+            ret, resp = send_request(query_url, RETRY_SEND_REQUEST)
+            if ret == 0:
+                try:
+                    resp_json = json.loads(resp)
+                    if resp_json["ret"] != 0:
+                        logging.error('server err = {err}, msg = {msg}'.format(err=resp_json["ret"], msg=resp_json["err_msg"]))
+                    else:
+                        return resp_json["data"]
+                except Exception as ex:
+                    logging.error(traceback.format_exc())
+            else:
+                logging.error('send_request failed: {ret}'.format(ret=ret))
+
+        except Exception as ex:
+            logging.error(traceback.format_exc())
+
+        logging.info(f'retry = {r}')
+        time.sleep(RETRY_FAILED_DELAY * r)
+
+    sys.exit(1)
+
 if __name__ == "__main__":
 
     options = webdriver.FirefoxOptions()
@@ -145,72 +175,23 @@ if __name__ == "__main__":
     now = datetime.now().timestamp()
 
     # get stock list
-    symbol_list = []
-    param = {
-        'code': afscreener_token,
-        'api': 'query-stock-list'
-    }
-    encoded_args = urlencode(param)
-    query_url = afscreener_url + '?' + encoded_args
-
-    ret, resp = send_request(query_url, RETRY_SEND_REQUEST)
-    if ret == 0:
-        try:
-            resp_json = json.loads(resp)
-            if resp_json["ret"] != 0:
-                logging.error('server err = {err}, msg = {msg}'.format(err=resp_json["ret"], msg=resp_json["err_msg"]))
-                sys.exit(1)
-            else:
-                symbol_list = resp_json["data"]
-
-        except Exception as ex:
-            logging.error(traceback.format_exc())
-    else:
-        logging.error('send_request failed: {ret}'.format(ret=ret))
-        sys.exit(1)
-
+    symbol_list = get_af_common_data('query-stock-list', RETRY_SEND_REQUEST)
     # get seg data
-    current_esg_data = {}
-    param = {
-        'code': afscreener_token,
-        'api': 'get-esg-data'
-    }
-    encoded_args = urlencode(param)
-    query_url = afscreener_url + '?' + encoded_args
+    current_esg_data = get_af_common_data('get-esg-data', RETRY_SEND_REQUEST)
+    # get recommendation data
+    current_recommendation_data = get_af_common_data('get-recommendation-data', RETRY_SEND_REQUEST)
 
-    ret, resp = send_request(query_url, RETRY_SEND_REQUEST)
-    if ret == 0:
-        try:
-            resp_json = json.loads(resp)
-            if resp_json["ret"] != 0:
-                logging.error('server err = {err}, msg = {msg}'.format(err=resp_json["ret"], msg=resp_json["err_msg"]))
-                sys.exit(1)
-            else:
-                current_esg_data = resp_json["data"]
-
-        except Exception as ex:
-            logging.error(traceback.format_exc())
-    else:
-        logging.error('send_request failed: {ret}'.format(ret=ret))
-        sys.exit(1)
-
-    output = {"data": {}}
+    output_esg = {"data": {}}
+    output_recommendation = {"data": {}}
     for s_i in range(len(symbol_list)):
         symbol = symbol_list[s_i]
-        logging.info(f'[{s_i+1} / {len(symbol_list)}] get {symbol} data [Updated: {len(output["data"])}]')
+        logging.info(f'[{s_i+1} / {len(symbol_list)}] get {symbol} data [ESG:({len(output_esg["data"])}) | Recomm:({len(output_recommendation["data"])})]')
 
-        if symbol in current_esg_data and now - UPDATE_INTERVAL < current_esg_data[symbol]["last_update_time"]:
+        esg_latest = symbol in current_esg_data and now - UPDATE_INTERVAL < current_esg_data[symbol]["last_update_time"]
+        recommendation_latest = symbol in current_recommendation_data and now - UPDATE_INTERVAL < current_recommendation_data[symbol]["last_update_time"]
+        if esg_latest and recommendation_latest:
             logging.info(f'no need update {symbol}')
             continue
-
-        output["data"][symbol] = {
-            "socialScore": "-",
-            "governanceScore": "-",
-            "environmentScore": "-",
-            "percentile": "-",
-            "totalEsg": "-",
-            "last_update_time": int(datetime.now().timestamp()),
-        }
 
         data = get_stock_data_by_browser(symbol, RETRY_SEND_REQUEST)
         if data is None:
@@ -221,25 +202,54 @@ if __name__ == "__main__":
             logging.warning('may occur encrypted data, skip going')
             break
 
-        d = stores["QuoteSummaryStore"]["esgScores"]
+        # parse esg data
+        if not esg_latest:
+            d = stores["QuoteSummaryStore"]["esgScores"]
+            output_esg["data"][symbol] = {
+                "socialScore": "-",
+                "governanceScore": "-",
+                "environmentScore": "-",
+                "percentile": "-",
+                "totalEsg": "-",
+                "last_update_time": int(datetime.now().timestamp()),
+            }
 
-        if len(d) > 0:
-            if "socialScore" in d and d["socialScore"] and "raw" in d["socialScore"]:
-                output["data"][symbol]["socialScore"] = d["socialScore"]["raw"]
-            if "governanceScore" in d and d["governanceScore"] and "raw" in d["governanceScore"]:
-                output["data"][symbol]["governanceScore"] = d["governanceScore"]["raw"]
-            if "environmentScore" in d and d["environmentScore"] and "raw" in d["environmentScore"]:
-                output["data"][symbol]["environmentScore"] = d["environmentScore"]["raw"]
-            if "percentile" in d and d["percentile"] and "raw" in d["percentile"]:
-                output["data"][symbol]["percentile"] = d["percentile"]["raw"]
-            if "totalEsg" in d and d["totalEsg"] and "raw" in d["totalEsg"]:
-                output["data"][symbol]["totalEsg"] = d["totalEsg"]["raw"]
-        else:
-            logging.info(f'no ESG update {symbol}')
+            if len(d) > 0:
+                if "socialScore" in d and d["socialScore"] and "raw" in d["socialScore"]:
+                    output_esg["data"][symbol]["socialScore"] = d["socialScore"]["raw"]
+                if "governanceScore" in d and d["governanceScore"] and "raw" in d["governanceScore"]:
+                    output_esg["data"][symbol]["governanceScore"] = d["governanceScore"]["raw"]
+                if "environmentScore" in d and d["environmentScore"] and "raw" in d["environmentScore"]:
+                    output_esg["data"][symbol]["environmentScore"] = d["environmentScore"]["raw"]
+                if "percentile" in d and d["percentile"] and "raw" in d["percentile"]:
+                    output_esg["data"][symbol]["percentile"] = d["percentile"]["raw"]
+                if "totalEsg" in d and d["totalEsg"] and "raw" in d["totalEsg"]:
+                    output_esg["data"][symbol]["totalEsg"] = d["totalEsg"]["raw"]
+            else:
+                logging.info(f'{symbol} no ESG update')
 
-        if len(output["data"]) >= BATCH_UPDATE:
-            update_db(output)
+            if len(output_esg["data"]) >= BATCH_UPDATE:
+                update_db(output_esg, 'update-esg-data')
 
-    update_db(output)
+        # parse recommendation data
+        if not recommendation_latest:
+            d = stores["QuoteSummaryStore"]["financialData"]
+            output_recommendation["data"][symbol] = {
+                "recommendationMean": "-",
+                "last_update_time": int(datetime.now().timestamp()),
+            }
+
+            if len(d) > 0:
+                if "recommendationMean" in d and d["recommendationMean"] and "raw" in d["recommendationMean"]:
+                    output_recommendation["data"][symbol]["recommendationMean"] = d["recommendationMean"]["raw"]
+            else:
+                logging.info(f'{symbol} no recommendation update')
+
+            if len(output_recommendation["data"]) >= BATCH_UPDATE:
+                update_db(output_recommendation, 'update-recommendation-data')
+
+    # final update
+    update_db(output_esg, 'update-esg-data')
+    update_db(output_recommendation, 'update-recommendation-data')
 
     logging.info('all task done')
